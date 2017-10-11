40b6885764d927fbee437f480c213861b61b4c0d
Seth Huffman <huffman.seth@gmail.com>
2017-10-11 19:08:31 -0400

    add delete button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function onReady ();
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-
+  // add event listener to form
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
 
@@ -10,11 +10,22 @@ function onReady ();
     let title = newToDoText.value;
 
     // create a new li
-    let checkbox = document.createElement('li');
+    let newLi = document.createElement('li');
 
     //create a new input
     let checkbox = document.createElement('input');
 
+    // create delete button
+    let deleteToDo = document.createElement('form');
+    deleteToDo.setAttribute('type', 'button');
+    deleteToDo.setAttribute('value', 'delete');
+    deleteToDo.setAttribute('id', 'deleteButton');
+    deleteToDo.setAttribute('class', 'deleteButton');
+
+    // add event listener for delete button
+    deleteToDo.addEventListener('click', event => {
+      var deleted = toDoList.removeChild(newLi);
+    }
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
@@ -23,6 +34,9 @@ function onReady ();
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+
+    // attach the delete button to the li
+    newLi.appendChild(deleteToDo);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
@@ -33,6 +47,5 @@ function onReady ();
 }
 
 window.onload = function () {
-  alert("The window has loaded!");
   onReady();
 };
